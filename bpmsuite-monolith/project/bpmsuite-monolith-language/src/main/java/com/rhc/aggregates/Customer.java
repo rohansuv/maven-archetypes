7db3aef9e72a29bb4d99dc94bdc78dc839335dcf
mlupton <mlupton@localhost.localdomain>
2015-12-16 10:07:34 -0600

    Added Customer object and com.rhc.aggregates package

---
+++ bpmsuite-monolith/project/bpmsuite-monolith-language/src/main/java/com/rhc/aggregates/Customer.java
@@ -0,0 +1,20 @@
+package com.rhc.aggregates;
+
+public class Customer {
+	
+	private String firstName;
+	private String lastName;
+	private Address address;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public Address getAddress() {
+		return address;
+	}
+}
